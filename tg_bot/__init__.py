8ec3b0b2f88a579f2c6a09cb6af0241eed00b499
MuhammedRk <70426191+MuhammedRk@users.noreply.github.com>
2021-07-30 15:46:53 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -54,7 +54,7 @@ if ENV:
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
-    SUPPORT_CHAT = "MyAwesomeSupportGroupGoesHere"
+    SUPPORT_CHAT = os.environ.get('DONATION_LINK', 'MyAwesomeSupportGroupGoesHere')
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
